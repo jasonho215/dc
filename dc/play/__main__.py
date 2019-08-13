8a8d5361066f187413adaa426a9b030ba23f87ab
Louis Chan <louischan0325@gmail.com>
2019-08-13 22:54:37 +0800

    WIP

--- dc/play/__main__.py
+++ dc/play/__main__.py
@@ -34,7 +34,7 @@ async def main():
                 "year": integer,
                 # full_council, committee, working_group
                 "meeting_type": keyword,
-                "meeting_number": text,
+                "meeting_number": keyword,
                 "meeting_date": date,
                 "meeting_location": text,
                 "content_type": keyword,
@@ -58,19 +58,10 @@ async def main():
             },
         },
     })
-    await client.index_document(index_name, "1", {
-        "district": "tuen_mun",
-        "year": 2019,
-        "meeting_type": "full_council",
-        "meeting_number": "第二十次",
-        "meeting_date": "2019-01-08T09:30:00+08:00",
-        "meeting_location": "屯門屯喜路1號\n屯門政府合署3樓\n屯門區議會會議室",
-        "content_type": "application/pdf",
-        "url": "https://www.districtcouncils.gov.hk/tm/doc/2016_2019/tc/dc_meetings_minutes/dc_20th_minutes_20190108.pdf",
-        "document_type": "minutes",
-        "page_number": 3,
-        "page_content": "主席歡迎各與會者出席屯門區議會第二十次會議，以及各政府部門代表列席會議。他表示， 政府會繼續安排部門首長輪流出席區議會會議，以加強溝通。出席是次會議的兩位部門首長分別為規劃署署長李啟榮先生及地政總署署長陳松青先生。",
-    })
+    with open("sample/data.json") as f:
+        data = json.load(f)
+        for idx, item in enumerate(data):
+            await client.index_document(index_name, str(idx), item)
     await client.refresh_index(index_name)
 
     q = "區議會 陳松青"
@@ -92,6 +83,41 @@ async def main():
                 "meeting_number": {},
                 "meeting_location": {},
                 "page_content": {},
+            },
+        },
+        "aggs": {
+            "by_district": {
+                "terms": {
+                    "field": "district",
+                },
+                "aggs": {
+                    "by_year": {
+                        "terms": {
+                            "field": "year",
+                        },
+                        "aggs": {
+                            "by_meeting_type": {
+                                "terms": {
+                                    "field": "meeting_type",
+                                },
+                                "aggs": {
+                                    "by_meeting_number": {
+                                        "terms": {
+                                            "field": "meeting_number",
+                                        },
+                                        "aggs": {
+                                            "by_document_type": {
+                                                "terms": {
+                                                    "field": "document_type",
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
             },
         },
     })
