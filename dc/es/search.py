e5fcba828e24e31cfa53d3d3833814ac7410f887
Louis Chan <louischan0325@gmail.com>
2019-08-13 23:47:59 +0800

    WIP

---
+++ dc/es/search.py
@@ -0,0 +1,162 @@
+from .client import ESClient, ESError
+
+keyword = {"type": "keyword"}
+integer = {"type": "integer"}
+date = {"type": "date"}
+text = {
+    "type": "text",
+    "analyzer": "icu_analyzer",
+    # Allow highlights
+    "index_options": "offsets",
+}
+
+
+class SearchClient:
+    def __init__(self, base_url):
+        self.client = ESClient(base_url)
+        self.index_name = "document"
+
+    async def recreate_index(self):
+        try:
+            await self.client.delete_index(self.index_name)
+        except ESError:
+            pass
+        await self.client.create_index(self.index_name, {
+            "mappings": {
+                "properties": {
+                    "district": keyword,
+                    "year": integer,
+                    # full_council, committee, working_group
+                    "meeting_type": keyword,
+                    "meeting_number": keyword,
+                    "meeting_date": date,
+                    "meeting_location": text,
+                    "content_type": keyword,
+                    "url": keyword,
+                    # agenda, minutes, audio
+                    "document_type": keyword,
+                    # if meeting_type == committee
+                    "committee_name": text,
+                    # if meeting_type == working_group
+                    "wg_name": text,
+                    # if document_type == (agenda || minutes)
+                    "page_number": integer,
+                    # if document_type == (agenda || minutes)
+                    "page_content": text,
+                    # if document_type == audio
+                    "agenda_code": keyword,
+                    # if document_type == audio
+                    "agenda_title": text,
+                    # if document_type == audio
+                    "duration": integer,
+                },
+            },
+        })
+
+    async def index_document(self, id, body):
+        await self.client.index_document(self.index_name, id, body)
+
+    async def refresh_index(self):
+        await self.client.refresh_index(self.index_name)
+
+    def _keyword_to_should(self, keyword):
+        terms = keyword.split(" ")
+        should = []
+        for term in terms:
+            term = term.strip()
+            should.append({"match_phrase": {"meeting_number": term}})
+            should.append({"match_phrase": {"meeting_location": term}})
+            should.append({"match_phrase": {"page_content": term}})
+        return should
+
+    async def search1(self, *, keyword):
+        should = self._keyword_to_should(keyword)
+        return await self.client.search_document(self.index_name, {
+            "_source": {
+                "excludes": ["page_content"],
+            },
+            "query": {
+                "bool": {
+                    "should": should,
+                },
+            },
+            "aggs": {
+                "by_district": {
+                    "terms": {
+                        "field": "district",
+                    },
+                    "aggs": {
+                        "by_year": {
+                            "terms": {
+                                "field": "year",
+                            },
+                            "aggs": {
+                                "by_meeting_type": {
+                                    "terms": {
+                                        "field": "meeting_type",
+                                    },
+                                    "aggs": {
+                                        "by_meeting_number": {
+                                            "terms": {
+                                                "field": "meeting_number",
+                                            },
+                                            "aggs": {
+                                                "by_document_type": {
+                                                    "terms": {
+                                                        "field": "document_type",
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        })
+
+    def interpret_search1_result(self, result):
+        output = []
+        for b1 in result["aggregations"]["by_district"]["buckets"]:
+            district = b1["key"]
+            for b2 in b1["by_year"]["buckets"]:
+                year = b2["key"]
+                for b3 in b2["by_meeting_type"]["buckets"]:
+                    meeting_type = b3["key"]
+                    for b4 in b3["by_meeting_number"]["buckets"]:
+                        meeting_number = b4["key"]
+                        count_by_document_type = {}
+                        for b5 in b4["by_document_type"]["buckets"]:
+                            document_type = b5["key"]
+                            count = b5["doc_count"]
+                        count_by_document_type[document_type] = count
+                        output.append({
+                            "district": district,
+                            "year": year,
+                            "meeting_type": meeting_type,
+                            "meeting_number": meeting_number,
+                            "count_by_document_type": count_by_document_type,
+                        })
+        return output
+
+    async def search2(self, *, keyword, district, year, meeting_type, meeting_number):
+        should = self._keyword_to_should(keyword)
+        return await self.client.search_document(self.index_name, {
+            "_source": {
+                "excludes": ["page_content"],
+            },
+            "query": {
+                "bool": {
+                    "should": should,
+                },
+            },
+            "highlight": {
+                "fields": {
+                    "meeting_number": {},
+                    "meeting_location": {},
+                    "page_content": {},
+                },
+            },
+        })
--- dc/play/__main__.py
+++ dc/play/__main__.py
@@ -1,20 +1,9 @@
 import asyncio
 import json
 
-from ..es import ESClient, ESError
+from ..es.search import SearchClient
 
 base_url = "http://localhost:9200"
-index_name = "document"
-
-keyword = {"type": "keyword"}
-integer = {"type": "integer"}
-date = {"type": "date"}
-text = {
-    "type": "text",
-    "analyzer": "icu_analyzer",
-    # Allow highlights
-    "index_options": "offsets",
-}
 
 
 def print_result(result):
@@ -22,106 +11,26 @@ def print_result(result):
 
 
 async def main():
-    client = ESClient(base_url)
-    try:
-        await client.delete_index(index_name)
-    except ESError:
-        pass
-    await client.create_index(index_name, {
-        "mappings": {
-            "properties": {
-                "district": keyword,
-                "year": integer,
-                # full_council, committee, working_group
-                "meeting_type": keyword,
-                "meeting_number": keyword,
-                "meeting_date": date,
-                "meeting_location": text,
-                "content_type": keyword,
-                "url": keyword,
-                # agenda, minutes, audio
-                "document_type": keyword,
-                # if meeting_type == committee
-                "committee_name": text,
-                # if meeting_type == working_group
-                "wg_name": text,
-                # if document_type == (agenda || minutes)
-                "page_number": integer,
-                # if document_type == (agenda || minutes)
-                "page_content": text,
-                # if document_type == audio
-                "agenda_code": keyword,
-                # if document_type == audio
-                "agenda_title": text,
-                # if document_type == audio
-                "duration": integer,
-            },
-        },
-    })
+    client = SearchClient(base_url)
+    await client.recreate_index()
     with open("sample/data.json") as f:
         data = json.load(f)
         for idx, item in enumerate(data):
-            await client.index_document(index_name, str(idx), item)
-    await client.refresh_index(index_name)
-
-    q = "區議會 陳松青"
-    terms = q.split(" ")
-    should = []
-    for term in terms:
-        term = term.strip()
-        should.append({"match_phrase": {"meeting_number": term}})
-        should.append({"match_phrase": {"meeting_location": term}})
-        should.append({"match_phrase": {"page_content": term}})
-    result = await client.search_document(index_name, {
-        "query": {
-            "bool": {
-                "should": should,
-            },
-        },
-        "highlight": {
-            "fields": {
-                "meeting_number": {},
-                "meeting_location": {},
-                "page_content": {},
-            },
-        },
-        "aggs": {
-            "by_district": {
-                "terms": {
-                    "field": "district",
-                },
-                "aggs": {
-                    "by_year": {
-                        "terms": {
-                            "field": "year",
-                        },
-                        "aggs": {
-                            "by_meeting_type": {
-                                "terms": {
-                                    "field": "meeting_type",
-                                },
-                                "aggs": {
-                                    "by_meeting_number": {
-                                        "terms": {
-                                            "field": "meeting_number",
-                                        },
-                                        "aggs": {
-                                            "by_document_type": {
-                                                "terms": {
-                                                    "field": "document_type",
-                                                },
-                                            },
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    })
-    print_result(result)
+            await client.index_document(str(idx), item)
+    await client.refresh_index()
+    keyword = "陳松青"
+    result = await client.search1(keyword=keyword)
+    interpreted_result = client.interpret_search1_result(result)
+    print_result(interpreted_result)
+    for item in interpreted_result:
+        r = await client.search2(
+            keyword=keyword,
+            district=item["district"],
+            year=item["year"],
+            meeting_type=item["meeting_type"],
+            meeting_number=item["meeting_number"],
+        )
+        print_result(r)
 
 
 asyncio.run(main())
