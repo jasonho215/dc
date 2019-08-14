fea48262cd13724ec61bc1a9e3edf5b2823cf289
Louis Chan <louischan0325@gmail.com>
2019-08-15 01:10:48 +0800

    Rename file

--- parser/src/main.js
+++ parser/src/main.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const { doIfNeed } = require("./utils");
-const read = require("./full-council.js");
+const read = require("./fullCouncil.js");
 const download = require("./downloadPDF");
 
 async function main() {
-  fs.mkdirSync("data", {recursive: true});
+  fs.mkdirSync("data", { recursive: true });
   await doIfNeed(read, "data/full-council.json");
   const items = JSON.parse(fs.readFileSync("data/full-council.json", { "encoding": "utf8" }));
   await download(items);
