9db0ad85223d52439f3eae70e9b1ee403460dfed
Louis Chan <louischan@oursky.com>
2019-08-14 21:46:29 +0800

    WIP - parser

--- parser/src/full-council.js
+++ parser/src/full-council.js
@@ -1,8 +1,29 @@
 const puppeteer = require("puppeteer");
-const { parseLink } = require("./utils");
+const { parseLink, parseDate } = require("./utils");
 
 const MEETING_COL_CNT = 7;
 const CIRCULATE_COL_CNT = 2;
+
+const DISTRICTS = [
+  // "central",
+  // "wc",
+  // "south",
+  // "east",
+  // "kt",
+  // "ssp",
+  // "ytm",
+  // "wts",
+  // "kc",
+  // "island",
+  // "tw",
+  // "yl",
+  // "north",
+  // "st",
+  // "sk",
+  // "kwt",
+  // "tp",
+  "tm",
+];
 
 async function parseTable(table) {
   const rows = await table.$$eval("tr", trs => {
@@ -33,29 +54,37 @@ async function parseTable(table) {
     }
     meetingList.push({
       name: d[0],
+      date: parseDate(d[1], d[2]),
       agenda: parseLink(d[3]),
       minutes: parseLink(d[4]),
-      audio: parseLink(d[5]),
-      discussion_papers: parseLink(d[6]),
-      circulates,
+      // audio: parseLink(d[5]),
     });
     i += circulates.length + 1;
   }
-  return meetingList;
+  return meetingList.filter(a => !!a.agenda && !!a.minutes);
 }
 
-(async () => {
+async function main() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(
-    "https://www.districtcouncils.gov.hk/tm/tc_chi/meetings/dcmeetings/dc_meetings.php"
-  );
-  const tables = Array.from(await page.$$("div#mainContent div[id^=table]"));
-  const result = {};
-  for (const table of tables) {
-    const id = String(await (await table.getProperty("id")).jsonValue());
-    result[id.slice(5)] = await parseTable(table);
+  for (const district of DISTRICTS) {
+    await page.goto(
+      `https://www.districtcouncils.gov.hk/${district}/tc_chi/meetings/dcmeetings/dc_meetings.php`
+    );
+    const tables = Array.from(await page.$$("div#mainContent div[id^=table]"));
+    const result = [];
+    for (const table of tables) {
+      const id = String(await (await table.getProperty("id")).jsonValue());
+      const year = id.slice(5);
+      const r = await parseTable(table);
+      for (const a of r) {
+        a.year = year;
+      }
+      result.push(r);
+    }
+    console.log(result);
   }
-  console.log(result);
   await browser.close();
-})();
+}
+
+main();
--- parser/src/utils.js
+++ parser/src/utils.js
@@ -1,5 +1,11 @@
 const { URL } = require("url");
 
+const TIME_REGEX = /(上|下)午(\d+)時(\d+分|正)/u;
+
+function pad(s) {
+  return ("0" + s).slice(-2);
+}
+
 function parseLink(val) {
   try {
     return new URL(val).toString();
@@ -8,4 +14,21 @@ function parseLink(val) {
   }
 }
 
-module.exports = { parseLink };
+function parseDate(dateStr, timeStr) {
+  const date = dateStr.slice(0, 10);
+  const matches = TIME_REGEX.exec(timeStr);
+  console.log(timeStr);
+  let hour = parseInt(matches[2], 10);
+  let minutes = "";
+  if (matches[1] === "下") {
+    hour += 12;
+  }
+  if (matches[3] === "正") {
+    minutes = "00";
+  } else {
+    minutes = matches[3].slice(0, 2);
+  }
+  return `${date}T${pad(hour)}:${pad(minutes)}:00.000+08:00`;
+}
+
+module.exports = { parseLink, parseDate };
