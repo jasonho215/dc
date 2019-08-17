1ff502501f393f30166b23f73b39ea202cb4a38a
Louis Chan <louischan0325@gmail.com>
2019-08-17 19:58:37 +0800

    Handle some malformed duration

--- src/parseDuration.js
+++ src/parseDuration.js
@@ -1,20 +1,38 @@
 module.exports = function parseDuration(s) {
-  const parts = s.split(":");
+  // 0'32
+  // 0"32
+  // 0:32
+  // 2:15:32
+  // 2：15：32
   let hour = 0;
   let minute = 0;
   let second = 0;
-  switch (parts.length) {
-  case 3:
-    hour = parseInt(parts[0], 10);
-    minute = parseInt(parts[1], 10);
-    second = parseInt(parts[2], 10);
-    break;
-  case 2:
-    minute = parseInt(parts[0], 10);
-    second = parseInt(parts[1], 10);
-    break;
-  default:
-    throw new Error("invalid duration: " + JSON.stringify(s));
+
+  let match = null;
+
+  if (match = /^(\d+)(?:['"])(\d+)$/.exec(s)) {
+    minute = match[1];
+    second = match[2];
+  } else {
+    const parts = s.replace("：", ":").split(":");
+    switch (parts.length) {
+    case 3:
+      hour = parts[0];
+      minute = parts[1];
+      second = parts[2];
+      break;
+    case 2:
+      minute = parts[0];
+      second = parts[1];
+      break;
+    default:
+      throw new Error("invalid duration: " + JSON.stringify(s));
+    }
   }
+
+  hour = parseInt(hour, 10);
+  minute = parseInt(minute, 10);
+  second = parseInt(second, 10);
+
   return hour * 3600 + minute * 60 + second;
 }
